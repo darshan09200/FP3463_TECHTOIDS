6c7b3baad08965582fdc685bb6cc2ec7b20ff08f
Jayesh Khistria <c0880752@mylambton.ca>
2022-11-23 17:48:35 -0500

    added motorcycle class

---
+++ src/Motorcycle.java
@@ -0,0 +1,16 @@
+public class Motorcycle extends Vehicle{
+    private boolean sidecar;
+
+    public Motorcycle(String make, String plate, String color, String category, boolean sidecar) {
+        super(make, plate, color, category);
+        this.sidecar = sidecar;
+    }
+
+    public boolean isSidecar() {
+        return sidecar;
+    }
+
+    public void setSidecar(boolean sidecar) {
+        this.sidecar = sidecar;
+    }
+}
