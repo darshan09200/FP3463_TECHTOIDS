08bf92a54d1ce482a59245716b59d0a938e63e68
Jayesh Khistria <c0880752@mylambton.ca>
2022-11-23 17:04:38 -0500

    added testcases for manager, programmer and tester

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        new Employee("Serge", 1980, 99, "Test");
+        new Manager("Cindy",1974,80,6,20);
+        new Programmer("Paul",1993,75,3);
+        new Tester("Pierre",1987,50,124);
     }
 }
