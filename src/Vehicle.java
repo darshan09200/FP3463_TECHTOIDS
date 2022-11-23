41eeab4f11745886d60e4c8008e5df4bf3fd5d5f
Darshan Jain <c0872398@mylambton.ca>
2022-11-23 17:37:39 -0500

    added vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+    private String make;
+    private String plate;
+    private String color;
+    private String category;
+
+    public Vehicle(String make, String plate, String color, String category) {
+        this.make = make;
+        this.plate = plate;
+        this.color = color;
+        this.category = category;
+    }
+
+    
+}
