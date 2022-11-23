deb0280e648d3f44ff53c2b4c87cd49129421eb2
Jayesh Khistria <c0880752@mylambton.ca>
2022-11-23 16:55:17 -0500

    added getter and setter for manager

--- src/Manager.java
+++ src/Manager.java
@@ -2,10 +2,25 @@ public class Manager extends Employee{
     private int nbTravelDays;
     private int nbClients;
 
-    public Manager(String name, int birthYear, double occupationRate, String role, int nbTravelDays, int nbClients) {
-        super(name, birthYear, occupationRate, role);
+    public Manager(String name, int birthYear, double occupationRate, int nbTravelDays, int nbClients) {
+        super(name, birthYear, occupationRate, "Manager");
         this.nbTravelDays = nbTravelDays;
         this.nbClients = nbClients;
     }
-    
+
+    public int getNbTravelDays() {
+        return nbTravelDays;
+    }
+
+    public void setNbTravelDays(int nbTravelDays) {
+        this.nbTravelDays = nbTravelDays;
+    }
+
+    public int getNbClients() {
+        return nbClients;
+    }
+
+    public void setNbClients(int nbClients) {
+        this.nbClients = nbClients;
+    }
 }
