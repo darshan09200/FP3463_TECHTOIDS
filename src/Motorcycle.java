6c972ef5a9da2f5f959b814e706f69c7fffa6070
jayesh-khistria <115033392+jayesh-khistria@users.noreply.github.com>
2022-11-24 18:02:24 -0500

    added description to motorcycle class

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -13,4 +13,18 @@ public class Motorcycle extends Vehicle{
     public void setSidecar(boolean sidecar) {
         this.sidecar = sidecar;
     }
+
+    @Override
+    public String toString() {
+
+        String desc = "Employee has a motorcycle. \n\t- make: " + getMake() +
+                "\n\t- plate: "+ getPlate() + "\n\t- color: " + getColor() + "\n\t- category: " + getCategory();
+        if (isSidecar()) {
+            desc += "\n\t- with sidecar";
+        }
+        else {
+            desc += "\n\t- without sidecar";
+        }
+        return desc;
+    }
 }
