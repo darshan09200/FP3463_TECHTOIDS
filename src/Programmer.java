6cb86b9e16311ca0c6294ff7db216efb28e61602
Jayesh Khistria <c0880752@mylambton.ca>
2022-11-24 16:29:25 -0500

    added vehicle to programmer class

--- src/Programmer.java
+++ src/Programmer.java
@@ -1,13 +1,21 @@
 public class Programmer extends Employee {
     private int nbProjects;
 
-    public Programmer(String name, int birthYear, double occupationRate, int nbProjects) {
-        super(name, birthYear, occupationRate, "Programmer", null);
+    public Programmer(String name, int birthYear, double occupationRate, int nbProjects, Vehicle vehicle) {
+        super(name, birthYear, occupationRate, "Programmer", vehicle);
         this.nbProjects = nbProjects;
     }
 
+    public Programmer(String name, int birthYear, double occupationRate, int nbProjects) {
+        this(name, birthYear, occupationRate, nbProjects, null);
+    }
+
+    public Programmer(String name, int birthYear, int nbProjects, Vehicle vehicle) {
+        this(name, birthYear, 100, nbProjects, vehicle);
+    }
+
     public Programmer(String name, int birthYear, int nbProjects) {
-        this(name, birthYear, 100, nbProjects);
+        this(name, birthYear, 100, nbProjects, null);
     }
 
     public int getNbProjects() {
